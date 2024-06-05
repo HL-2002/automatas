9dda2a2e03691f17381311a25b3aeabcf0130a67
HL-2002 <henryslang8231682@gmail.com>
2024-06-05 17:12:05 -0400

    help

--- player.gd
+++ player.gd
@@ -2,14 +2,16 @@ extends CharacterBody2D
 
 class_name Player
 
-const SPEED = 300.0
+const SPEED = 200.0
 
 
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	# Get input's direction
 	var direction = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up","ui_down")
+
 	if  bool(direction) != bool(direction_y)  :
 		velocity.x = 0
 		velocity.y = 0
@@ -17,9 +19,7 @@ func _physics_process(delta):
 		velocity.y = direction_y * SPEED
 		$AnimatedSprite2D.play("walk")
 
-
 	move_and_slide()
-
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("enemy"):
